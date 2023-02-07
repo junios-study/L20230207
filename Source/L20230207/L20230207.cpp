53d4d21e74c42eb6481fc9dcafb502bb25830f28
junios <junios@naver.com>
2023-02-07 10:14:41 +0900

    first commit

---
+++ Source/L20230207/L20230207.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "L20230207.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, L20230207, "L20230207" );
